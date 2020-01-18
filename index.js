775227350e1b327044c2cbf62bee1d719bc8c017
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-18 12:53:12 +0900

    express middleware

--- index.js
+++ index.js
@@ -4,16 +4,26 @@ const app = express();
 
 const PORT = 4000;
 
-const handleListening = () => 
-  console.log(`Listening on: http://localhost:${PORT}`);
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
-const handleHome = (req, res) => 
-  res.send(`Welcome to Home!!`);
+const handleHome = (req, res) => res.send(`Welcome to Home!!`);
 
-const handleProfile = (req, res) => 
-  res.send(`You are on my Profile`);
- 
+const handleProfile = (req, res) => res.send(`You are on my Profile!`);
+//
+const middleWare = (req, res, next) => {
+  console.log(`I'm Middlewares.`);
+  next();
+}
+
+//특정 라우트만 미들웨어를 사용할 때!
+//app.get('/', middleWare, handleHome);
+
+//모든 라우트가 미들웨어를 사용하게 만들 때!
+app.use(middleWare);
+
 app.get('/', handleHome);
+// 상위 라우트를 제외할 때는 위치로 제어한다. 
+//app.use(middleWare);
 
 app.get('/profile', handleProfile);
  
