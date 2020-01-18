16a8b7d1f21acc70db6c38ca068b7ea93896149a
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-18 12:15:06 +0900

    install express.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hello node.js');
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+const handleHome = (req, res) => {
+  res.send(`Welcome to Home`);
+}
+
+const handleProfile = (req, res) => {
+  res.send(`You are on my Profile`);
+}
+ 
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+ 
+app.listen(PORT, handleListening);
