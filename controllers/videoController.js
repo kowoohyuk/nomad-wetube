07e94f3a93cee00429b8bc2d120c8e9afbfe9132
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-19 18:13:15 +0900

    layout with pug (main.pug)

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,10 +1,9 @@
 //render는 기본적으로 /views 폴더 내의 해당 파일을 찾아서 렌딩해준다.
 //app.js에서 뷰 엔진에 pug를 설정했기 때문에 아래의 코드는 home.pug를 찾게 된다.
 export const home = (req, res) => res.render('home');
-
-export const search = (req, res) => res.send('Search');
-export const videos = (req, res) => res.send('Videos');
-export const upload = (req, res) => res.send('Upload');
-export const videoDetail = (req, res) => res.send('VideoDetail');
-export const editVideo = (req, res) => res.send('EditVideo');
-export const deleteVideo = (req, res) => res.send('DeleteVideo');
+export const search = (req, res) => res.render('search');
+export const videos = (req, res) => res.render('videos');
+export const upload = (req, res) => res.render('upload');
+export const videoDetail = (req, res) => res.render('videoDetail');
+export const editVideo = (req, res) => res.render('editVideo');
+export const deleteVideo = (req, res) => res.render('deleteVideo');
