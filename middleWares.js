6425f31151663c80ddd920b1e3d1dff398e029cf
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-24 19:24:07 +0900

    modify controller, bodyParser remark

--- middleWares.js
+++ middleWares.js
@@ -4,5 +4,9 @@ export const localsMiddleWares = (req, res, next) => {
   //locals에 설정한 프로퍼티는 전역 변수처럼 사용 가능하다. main.pug에서 #{siteName}로 사용
   res.locals.siteName = 'WeTube';
   res.locals.routes = routes;
+  res.locals.user = {
+    isAuthenticated: true,
+    id: 1
+  }
   next();
 };
