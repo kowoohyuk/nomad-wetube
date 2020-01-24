b2a1264576b7e5f43ec01f3c85e6506d35700e9c
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-24 20:16:51 +0900

    install mongo, mongoose, dotenv

--- app.js
+++ app.js
@@ -13,10 +13,7 @@ const app = express();
 app.use(helmet());
 app.set('view engine', 'pug');
 app.use(cookieParser());
-
-//bodyParser를 통해 form의 값을 req의 body로 받아올 수 있다.
 app.use(bodyParser.json());
-//urlencoded도 필요하다!
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.use(logger("dev"));
