2298bd695b43c8d9cb3426089fd9e2969646505e
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-26 16:02:39 +0900

    install multer, add upload

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -19,14 +19,21 @@ export const search = (req, res) => {
 export const getUpload = (req, res) => {
   res.render("upload", { pageTitle: "Upload" });
 };
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
     body: {
-      file, title, description
+      title, description
+    },
+    file: {
+      path
     }
   } = req;
-  // To Do: 비디오 업로드, 저장
-  res.redirect(routes.videoDetail(324393));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description
+  });
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
