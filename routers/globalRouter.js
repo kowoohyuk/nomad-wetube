e9b97c3d84a406a511b0e7c0d48ce5880f9e04a8
kowoohyuk <kowoohyuk91@gmail.com>
2020-01-28 18:44:49 +0900

    tmp github oauth login

--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import passport from "passport";
 import routes from "../routes";
 import { home, search } from "../controllers/videoController";
 import {
@@ -6,7 +7,9 @@ import {
   getLogin,
   logout,
   postJoin,
-  postLogin
+  postLogin,
+  githubLogin,
+  postGithubLogin
 } from "../controllers/userController";
 import { onlyPublic } from "../middlewares";
 
@@ -21,5 +24,13 @@ globalRouter.post(routes.login, onlyPublic, postLogin);
 globalRouter.get(routes.home, home);
 globalRouter.get(routes.search, search);
 globalRouter.get(routes.logout, onlyPublic, logout);
+
+globalRouter.get(routes.gitHub, githubLogin);
+
+globalRouter.get(
+  routes.githubCallback,
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  postGithubLogin
+);
 
 export default globalRouter;
