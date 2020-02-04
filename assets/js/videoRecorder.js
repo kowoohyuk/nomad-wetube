fb4c84547393edfbfe875cb91fcfa976885718fc
kowoohyuk <kowoohyuk91@gmail.com>
2020-02-04 18:19:46 +0900

    tmp record

---
+++ assets/js/videoRecorder.js
@@ -0,0 +1,19 @@
+const recorderContainer = document.getElementById("jsRecordContainer");
+const recordBtn = document.getElementById("jsRecordBtn");
+const videoPreview = document.getElementById("jsVideoPreview");
+
+const startRecording = async () => {
+  try {
+    //
+  } catch (error) {
+    //
+  }
+};
+
+function init() {
+  recordBtn.addEventListener("click", startRecording);
+}
+
+if (recorderContainer) {
+  init();
+}
