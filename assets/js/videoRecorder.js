a9cdeac67322c52b1d39387bb22544307e055040
kowoohyuk <kowoohyuk91@gmail.com>
2020-02-07 18:27:34 +0900

    tmp recorder

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,7 +2,15 @@ const recorderContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
+let streamObject;
+
 const startRecording = async () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  console.log(videoRecorder);
+  videoRecorder.start();
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
@@ -11,14 +19,18 @@ const startRecording = async () => {
     videoPreview.srcObject = stream;
     videoPreview.muted = true;
     videoPreview.onplay();
+    recordBtn.textContent = "Stop recording";
+    streamObject = stream;
+    startRecording();
   } catch (error) {
-    recordBtn.textContent = " Can not record";
-    recordBtn.removeEventListener("click", startRecording);
+    recordBtn.textContent = "Can not record";
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recorderContainer) {
